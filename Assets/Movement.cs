242e94491d0a3b0e2430b632d7e6befe6a9608e5
GanzaMos <moskalik.ganza@gmail.com>
2020-07-17 20:07:47 +0300

    2 prototype first level and add enemy with navmesh

---
+++ Assets/Movement.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Movement : MonoBehaviour
+{
+    [SerializeField] private Transform _targetToMove;
+    NavMeshAgent _navMeshAgent;
+    void Start()
+    {
+        _navMeshAgent = GetComponent<NavMeshAgent>();
+    }
+    
+    void Update()
+    {
+        _navMeshAgent.SetDestination(_targetToMove.position);
+    }
+}
