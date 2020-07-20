46bed8a260a8dda3f2ca1ece1f1e2881348126d8
GanzaMos <moskalik.ganza@gmail.com>
2020-07-20 17:10:44 +0300

    4 enemy's helth system

---
+++ Assets/EnemyHealth.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyHealth : MonoBehaviour
+{
+
+    [SerializeField] private int _enemyHealth = 5;
+
+    public void DecreaseHealth(int _powerOfHit)
+    {
+        _enemyHealth -= _powerOfHit;
+        if (_enemyHealth <= 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
--- Assets/WeaponScript.cs
+++ Assets/WeaponScript.cs
@@ -1,18 +1,39 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 
 public class WeaponScript : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
+    [SerializeField] Camera FPCamera;
+    [SerializeField] float _range;
+    [SerializeField] int _powerOfHit = 1;
     void Update()
     {
-        
+        if (Input.GetButtonDown("Fire1"))
+        {
+            Shoot();
+        }
+    }
+
+    private void Shoot()
+    {
+        RaycastHit hit;
+        if (Physics.Raycast(FPCamera.transform.position, FPCamera.transform.forward, out hit, _range))
+        {
+            EnemyHealth target = hit.transform.GetComponent<EnemyHealth>();
+            if (target)
+            {
+                target.DecreaseHealth(_powerOfHit);
+            }
+            
+            //todo make some visual effects for hitting the enemy;
+            //todo call a method to decreases enemy health;
+        }
+        else
+        {
+            
+        }
     }
 }
