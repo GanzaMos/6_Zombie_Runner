ebd4e73b1ac0c224ca21e86de7d3b43b68b91822
GanzaMos <moskalik.ganza@gmail.com>
2020-07-20 15:46:08 +0300

    3 weapon and reticle

--- Assets/Movement.cs
+++ Assets/Movement.cs
@@ -1,12 +1,17 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.Text;
 using UnityEngine;
 using UnityEngine.AI;
 
 public class Movement : MonoBehaviour
 {
     [SerializeField] private Transform _targetToMove;
+    [SerializeField] float _detectRadius = 10;
     NavMeshAgent _navMeshAgent;
+    private bool _isProvoke = false;
+    private float _distanceToTarget;
     void Start()
     {
         _navMeshAgent = GetComponent<NavMeshAgent>();
@@ -14,6 +19,58 @@ public class Movement : MonoBehaviour
     
     void Update()
     {
+        
+        CheckIsProvoke();
+        
+        if (_isProvoke == true)
+        {
+            EngageTarget();
+        }
+            
+    }
+
+    void CheckIsProvoke()
+    {
+        _distanceToTarget = Vector3.Distance(transform.position, _targetToMove.transform.position);
+
+        if (_distanceToTarget < _detectRadius)
+        {
+            _isProvoke = true;
+        }
+        else
+        {
+            _isProvoke = false;
+        }
+    }
+
+    private void EngageTarget()
+    {
+        if (_distanceToTarget < _detectRadius && _distanceToTarget >= _navMeshAgent.stoppingDistance)
+        {
+            ChaseTarget();
+        }
+
+        if (_distanceToTarget < _navMeshAgent.stoppingDistance)
+        {
+            AttackTarget();
+        }
+
+
+    }
+
+    private static void AttackTarget()
+    {
+        Debug.Log("Attack target");
+    }
+
+    private void ChaseTarget()
+    {
         _navMeshAgent.SetDestination(_targetToMove.position);
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, _detectRadius);
     }
 }
--- Assets/WeaponScript.cs
+++ Assets/WeaponScript.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ana : MonoBehaviour
+public class WeaponScript : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
