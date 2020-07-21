cfed63f176d27bb819f8f70436c57e2d30bc9fb3
GanzaMos <moskalik.ganza@gmail.com>
2020-07-21 21:10:58 +0300

    9 make some fancy UA elements

---
+++ Assets/GameSessionScript.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameSessionScript : MonoBehaviour
+{
+    public void LoadLevel()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+}
