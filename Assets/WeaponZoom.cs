eb142475fbb56ec48a029786767cc7dba6a52be9
GanzaMos <moskalik.ganza@gmail.com>
2020-07-28 20:04:06 +0300

    18 sensitivity decreases when zooming weapon

--- Assets/WeaponZoom.cs
+++ Assets/WeaponZoom.cs
@@ -1,19 +1,44 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityStandardAssets.Characters.FirstPerson;
 
 public class WeaponZoom : MonoBehaviour
 {
+    [SerializeField] private float _standartView = 45f;
+    [SerializeField] private float _zoomView = 20f;
+    [Range(0f,1f)][SerializeField] private float _declineSensitivity = 0.5f;
+
+
+    private Camera camera;
+    private RigidbodyFirstPersonController controller;
+    private float _startXSensitivity;
+    private float _startYSensitivity;
+    
+    private void Start()
+    {
+        camera = GetComponentInChildren<Camera>();
+        controller = GetComponent<RigidbodyFirstPersonController>();
+        _startXSensitivity = controller.mouseLook.XSensitivity;
+        _startYSensitivity = controller.mouseLook.YSensitivity;
+    }
 
     void Update()
     {
         if (Input.GetButton("Fire2"))
         {
-            GetComponentInChildren<Camera>().fieldOfView = 20;
+            camera.fieldOfView = _zoomView;
+            
+            controller.mouseLook.XSensitivity = _startXSensitivity * _declineSensitivity;
+            controller.mouseLook.YSensitivity = _startYSensitivity * _declineSensitivity;
         }
         else
         {
-            GetComponentInChildren<Camera>().fieldOfView = 45;
+            camera.fieldOfView = _standartView;
+            
+            controller.mouseLook.XSensitivity = _startXSensitivity;
+            controller.mouseLook.YSensitivity = _startYSensitivity;
         }
     }
 }
