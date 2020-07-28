07e92847ae830e6f1b7a2869a2cdfe71f62ac7ba
GanzaMos <moskalik.ganza@gmail.com>
2020-07-28 19:26:01 +0300

    17 massive level update + weapon zoom

---
+++ Assets/WeaponZoom.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponZoom : MonoBehaviour
+{
+
+    void Update()
+    {
+        if (Input.GetButton("Fire2"))
+        {
+            GetComponentInChildren<Camera>().fieldOfView = 20;
+        }
+        else
+        {
+            GetComponentInChildren<Camera>().fieldOfView = 45;
+        }
+    }
+}
