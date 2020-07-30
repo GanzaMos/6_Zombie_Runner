e415f86595495f0c9ba76594d9c9d5fb3b211fcd
GanzaMos <moskalik.ganza@gmail.com>
2020-07-30 21:24:42 +0300

    30 Weapon pickups and simple trigger collision which interacts with player

---
+++ Assets/PickUpScript.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickUpScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.transform.GetComponent<PlayerHealth>())
+        {
+            //MakePickUpStuff();
+            Destroy(gameObject);
+        }
+    }
+}
